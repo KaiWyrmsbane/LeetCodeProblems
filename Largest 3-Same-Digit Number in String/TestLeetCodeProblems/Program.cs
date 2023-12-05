c7b6f6bae2d42a9945871d24bf04153a908074f7
James VanMeter <jamesvanmeter1219@gmail.com>
2023-12-04 19:33:36 -0500

    Working on my first LeetCode Problem

---
+++ Largest 3-Same-Digit Number in String/TestLeetCodeProblems/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        trythis("001255543");
+    }
+
+    public static void trythis(string num)
+    {
+        //build a list to store all numbers in
+        List<int> numList = new List<int>();
+        int.Parse(num);
+        string subString = "";
+        foreach (int number in num)
+        {
+            var temp = number - 48;
+            numList.Add(temp);
+            Console.Write(temp);
+        }
+        foreach (int number in numList)
+        {
+            if (numList[number] != 0)
+            { 
+                bool isEqual = numList[number] == numList[number - 1];
+                if (isEqual == true)
+                {
+                    Console.WriteLine($"{number} has potential");
+                    var possibleSubString = number.ToString(); 
+                    var builder = new StringBuilder();
+                    foreach(var c in possibleSubString)
+                    {
+                        builder.Append(c);
+                        if(builder.Length == 3)
+                        {
+                            subString = builder.ToString();
+                        }
+                    }
+                    
+                }
+            }
+           // Console.WriteLine(number);
+        }
+
+        Console.WriteLine(subString);
+    }
+}
